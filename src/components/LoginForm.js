2fb1889ba3cfc07d38c8a5fd69e7f5979d49b82b
Joshua Wood <JerichoWall@apu.edu>
2019-03-11 10:20:51 -0700

    Basic Framework and Login set up

---
+++ src/components/LoginForm.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      userType: ''
+    }
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name] : e.target.value })
+  }
+
+  render() {
+    
+  return (
+    <form>
+
+      <label>Username</label>
+      <input 
+        type="text"
+        placeholder='Username...'
+        name='username'
+        value={this.state.username}
+        onChange={this.handleChange}
+        required
+      />
+      
+      <label>Password</label>
+      <input 
+        type="password" 
+        placeholder='Password' 
+        name="password"
+        value={this.state.password}
+        onChange={this.handleChange}
+        required
+      />
+
+      {/* <h2>{props.userType === Guide ? "Guide" : "Tourist"}</h2> */}
+
+    </form>
+  )
+  }
+}
+
+export default LoginForm;
---
+++ src/components/RouteLogin.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import LoginButton from './LoginButton';
+import LoginForm from './LoginForm';
+
+const RouteLogin = props => {
+    return (
+        <div>
+            <h1>Wanderlust!</h1>
+            <LoginForm />
+            <LoginButton  loginButtonText="Tourist"/>
+            <LoginButton  loginButtonText="Guide"/>
+            
+        </div>
+    )
+}
+
+export default RouteLogin;
