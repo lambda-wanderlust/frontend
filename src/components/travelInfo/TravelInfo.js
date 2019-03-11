9265a92aaf58ff77db82abeb4e40de9e6514bfd5
Joshua Wood <JerichoWall@apu.edu>
2019-03-11 14:27:45 -0700

    Update framework for tourinfo and guides

---
+++ src/components/travelInfo/TravelInfo.js
@@ -0,0 +1,44 @@
+import React from "react";
+import axios from "axios";
+import { Route, Link } from 'react-router-dom';
+import TravelCard from "./TravelCard";
+import CreateExp from './CreateExp';
+import UpdateExp from './UpdateExp';
+
+class TravelInfo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            trips: [],
+        };
+    }
+
+    componentDidMount() {
+        axios.get('https://lambda-wanderlust-backend.herokuapp.com/')
+            .then(res => {
+                console.log(res)
+                this.SetState({ trips: res.trips })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+  
+    render() {
+        return (
+            <div>
+                {this.state.trips.map(trip => {
+                    return (
+                        <TravelCard
+                            trip={trip}
+                        />
+                    )
+                })}
+                <Route path="/guides/createexp" component={CreateExp} />
+                <Route path="/guides/updateexp" component={UpdateExp} />
+            </div>
+        );
+    }
+}
+
+export default TravelInfo;
