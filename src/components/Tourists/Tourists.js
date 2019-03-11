89f2dd3005549b3621d6e34aff10a47379653e62
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-03-11 14:36:01 -0400

    Created Tourists component for displaying data

---
+++ src/components/Tourists/Tourists.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Tourists extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <p>test</p>
+      </div>
+    );
+  }
+}
+
+export default Tourists;
