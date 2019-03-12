f792b24261b252bd9cbb1eb5326841672ad5936b
Joshua Wood <JerichoWall@apu.edu>
2019-03-12 16:52:07 -0700

    Routing is nearly complete. One more small component to implement tomorrow

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ class App extends Component {
       <div className="App">
         <Route exact path="/" render={(props) => { return <RouteLogin props={this.state} userLogin={this.userLogin}/> }} />
         <Route path="/create-user-form" render={(props) => { return <CreateAccountForm props={this.state} userLogin={this.userLogin} /> }} />
-        <Route path="/travel-info" render={(props) => { return <TravelInfo props={this.state} /> }} />
-        <Route path="/travel-info/create-exp" component={CreateExp} />
+        <Route path="/travel-info" render={(props) => { return <TravelInfo {...props} props={this.state} /> }} />
+        <Route path="/create-exp" component={CreateExp} />
       </div>
     );
   }
--- src/components/travelInfo/TravelInfo.js
+++ src/components/travelInfo/TravelInfo.js
@@ -25,6 +25,7 @@ class TravelInfo extends React.Component {
   }
 
 createExperience = () => {
+    console.log(this.props)
     this.props.history.push("/create-exp");
 }
 
