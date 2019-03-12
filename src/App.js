2e04f1bba00e181c2b0ef8570201e1f203b94c9e
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-03-11 21:15:26 -0400

    Build out CreateExp Components and have framing up for CRUD. tested post and getting data logged

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
 import RouteLogin from "./components/RouteLogin";
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import TravelInfo from "./components/travelInfo/TravelInfo";
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-import CreateAccountForm from './components/createAccount/CreateAccountForm';
-
+import CreateAccountForm from "./components/createAccount/CreateAccountForm";
 
 class App extends Component {
   render() {
@@ -14,7 +12,7 @@ class App extends Component {
       <div className="App">
         <RouteLogin />
         <TravelInfo />
-        <Route exact path="/" component={RouteLogin}/>
+        <Route exact path="/" component={RouteLogin} />
         <Route exact path="/create-user-form" component={CreateAccountForm} />
       </div>
     );
--- src/components/travelInfo/CreateExp.js
+++ src/components/travelInfo/CreateExp.js
@@ -1,12 +1,112 @@
-import React from 'react';
+import React from "react";
 
-const CreateExp = props => {
+import axios from "axios";
+
+class CreateExp extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      location: "",
+      quantity: "",
+      units: "",
+      trip_type: "",
+      service_type: ""
+    };
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    let newTrip = {
+      location: this.state.location,
+      quantity: this.state.quantity,
+      units: this.state.units,
+      trip_type: this.state.trip_type,
+      service_type: this.state.service_type
+    };
+    e.preventDefault();
+    console.log(newTrip);
+    //axios
+    //.post(
+    //"https://lambda-wanderlust-backend.herokuapp.com/api/trips/",
+    //newTrip
+    //)
+    //.then(res => res.data)
+    //.catch(err => console.log(err));
+  };
+
+  handleUpdate = (e, id) => {
+    let updatedTrip = this.state;
+    e.preventDefault();
+    axios
+      .put(
+        "https://lambda-wanderlust-backend.herokuapp.com/api/trips/",
+        updatedTrip
+      )
+      .then(res => res.data)
+      .catch(err => console.log(err));
+  };
+
+  handleDelete = (e, id) => {
+    let deletePost = this.state;
+    e.preventDefault();
+    axios
+      .delete(
+        "https://lambda-wanderlust-backend.herokuapp.com/api/trips/",
+        deletePost
+      )
+      .then(res => res.data)
+      .catch(err => console.log(err));
+  };
+
+  render() {
     return (
-        <div>
-
-        </div>
-    )
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="What Location..."
+            name="location"
+            value={this.state.location}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            placeholder="What Quantity..."
+            name="quantity"
+            value={this.state.quantity}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            placeholder="What Units..."
+            name="units"
+            value={this.state.units}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            placeholder="What Trip Type..."
+            name="trip_type"
+            value={this.state.trip_type}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            placeholder="What Service Type..."
+            name="service_type"
+            value={this.state.service_type}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleSubmit}>Add Trip</button>
+        </form>
+        <button onClick={this.handleUpdate}>Update Trip Info</button>
+        <button onClick={this.deletePost}>Delete Trip</button>
+      </div>
+    );
+  }
 }
-
 
 export default CreateExp;
