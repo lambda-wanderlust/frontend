d6288c71ba790be078aa1a20cc0d08b83f73f287
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-03-13 12:40:27 -0400

    Created Spinner component and implementd in singleTripCard

--- src/components/travelInfo/SingleTripCard.js
+++ src/components/travelInfo/SingleTripCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Spinner from "../Spinner/Spinner.js";
 
 const SingleTripCard = props => {
   const { id } = props.match.params;
@@ -16,7 +17,7 @@ const SingleTripCard = props => {
       </div>
     );
   } else {
-    return <p>Loading...</p>;
+    return <Spinner />;
   }
 };
 
