5574fa2b3e0b5fd64ad25509b47238fe09d6b421
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-03-13 12:23:20 -0400

    fixed routing bug. on reload data not found . fixed with conditional possibly add spinner later on

--- src/components/travelInfo/SingleTripCard.js
+++ src/components/travelInfo/SingleTripCard.js
@@ -5,16 +5,19 @@ const SingleTripCard = props => {
   const trip = props.trips.find(thing => {
     return `${thing.id}` === id;
   });
-
-  return (
-    <div>
-      <p>{trip.location}</p>
-      <p>{trip.quantity}</p>
-      <p>{trip.units}</p>
-      <p>{trip.trip_type}</p>
-      <p>{trip.service_type}</p>
-    </div>
-  );
+  if (trip) {
+    return (
+      <div>
+        <p>{trip.location}</p>
+        <p>{trip.quantity}</p>
+        <p>{trip.units}</p>
+        <p>{trip.trip_type}</p>
+        <p>{trip.service_type}</p>
+      </div>
+    );
+  } else {
+    return <p>Loading...</p>;
+  }
 };
 
 export default SingleTripCard;
--- src/components/travelInfo/UpdateExp.js
+++ src/components/travelInfo/UpdateExp.js
@@ -1,117 +1,117 @@
-import React from "react";
+import React from 'react';
 
-import axios from "axios";
+import axios from 'axios';
 
 class CreateExp extends React.Component {
-    constructor(props) {
-        super();
-        this.state = {
-            location: "",
-            quantity: "",
-            units: "",
-            trip_type: "",
-            service_type: ""
-        };
-    }
+  constructor(props) {
+    super();
+    this.state = {
+      location: '',
+      quantity: '',
+      units: '',
+      trip_type: '',
+      service_type: '',
+    };
+  }
 
-    handleChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
+  handleChange = e => {
+    this.setState({[e.target.name]: e.target.value});
+  };
+
+  handleSubmit = e => {
+    let newTrip = {
+      location: this.state.location,
+      quantity: this.state.quantity,
+      units: this.state.units,
+      trip_type: this.state.trip_type,
+      service_type: this.state.service_type,
     };
 
-    handleSubmit = e => {
-        let newTrip = {
-            location: this.state.location,
-            quantity: this.state.quantity,
-            units: this.state.units,
-            trip_type: this.state.trip_type,
-            service_type: this.state.service_type
-        };
+    e.preventDefault();
+    console.log(newTrip);
+    axios
+    .post(
+    "https://lambda-wanderlust-backend.herokuapp.com/api/trips",
+    newTrip
+    )
+    .then(res => res.data)
+    .catch(err => console.log(err));
+  };
 
-        e.preventDefault();
-        console.log(newTrip);
-        //axios
-        //.post(
-        //"https://lambda-wanderlust-backend.herokuapp.com/api/trips",
-        //newTrip
-        //)
-        //.then(res => res.data)
-        //.catch(err => console.log(err));
-    };
-
-    handleUpdate = (e, id) => {
+  handleUpdate = (e, id) => {
     let updatedTrip = this.state;
-    console.log(this.state)
+    console.log(this.state);
     e.preventDefault();
     axios
-        .put(
-        "https://lambda-wanderlust-backend.herokuapp.com/api/trips",
-        updatedTrip
-        )
-        .then(res => {
-            console.log(res)
-        
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    };
+      .put(
+        'https://lambda-wanderlust-backend.herokuapp.com/api/trips',
+        updatedTrip,
+      )
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-    handleDelete = (e, id) => {
-        let deletePost = this.state;
-        e.preventDefault();
-        axios
-            .delete("https://lambda-wanderlust-backend.herokuapp.com/api/trips",
-            deletePost
-            )
-            .then(res => res.data)
-            .catch(err => console.log(err));
-    };
+  handleDelete = (e, id) => {
+    let deletePost = this.state;
+    e.preventDefault();
+    axios
+      .delete(
+        'https://lambda-wanderlust-backend.herokuapp.com/api/trips',
+        deletePost,
+      )
+      .then(res => res.data)
+      .catch(err => console.log(err));
+  };
 
-    render() {
-        return (
-            <div>
-                <form>
-                    <input
-                        type="text"
-                        placeholder="What Location..."
-                        name="location"
-                        value={this.state.location}
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        type="text"
-                        placeholder="What Quantity..."
-                        name="quantity"
-                        value={this.state.quantity}
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        type="text"
-                        placeholder="What Units..."
-                        name="units"
-                        value={this.state.units}
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        type="text"
-                        placeholder="What Trip Type..."
-                        name="trip_type"
-                        value={this.state.trip_type}
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        type="text"
-                        placeholder="What Service Type..."
-                        name="service_type"
-                        value={this.state.service_type}
-                        onChange={this.handleChange}
-                    />
-                </form>
-                <button onClick={this.handleUpdate}>Update Trip Info</button>
-                <button onClick={this.deletePost}>Delete Trip</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="What Location..."
+            name="location"
+            value={this.state.location}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            placeholder="What Quantity..."
+            name="quantity"
+            value={this.state.quantity}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            placeholder="What Units..."
+            name="units"
+            value={this.state.units}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            placeholder="What Trip Type..."
+            name="trip_type"
+            value={this.state.trip_type}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            placeholder="What Service Type..."
+            name="service_type"
+            value={this.state.service_type}
+            onChange={this.handleChange}
+          />
+        </form>
+        <button onClick={this.handleUpdate}>Update Trip Info</button>
+        <button onClick={this.deletePost}>Delete Trip</button>
+      </div>
+    );
+  }
 }
 
 export default CreateExp;
