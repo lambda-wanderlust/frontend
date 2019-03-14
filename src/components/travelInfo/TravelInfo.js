f817471c207f55867310f8a2ca624293aef2ed8b
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-03-14 14:31:19 -0400

    Merge nightmare

--- src/components/travelInfo/TravelInfo.js
+++ src/components/travelInfo/TravelInfo.js
@@ -16,11 +16,6 @@ const CardContainer = styled.div`
 `;
 
 const CardWrapper = styled.div`
-  <<<<<<<headborder: 2px solid black;
-  margin: 10px;
-`;
-
-const CardWrapper = styled.div`
   border: 2px solid black;
   margin: 10px;
   width: 30%;
@@ -47,7 +42,8 @@ class TravelInfo extends React.Component {
         service_type: ""
       },
       search: "",
-      filteredTrips: []
+      filteredTrips: [],
+      numTripsToDisplay: 5
     };
   }
 
@@ -138,7 +134,10 @@ class TravelInfo extends React.Component {
             exact
             path="/travel-info"
             render={props => {
-              return this.state.filteredTrips.map(trip => {
+              return this.state.filteredTrips.map((trip, index) => {
+                if (index > this.state.numTripsToDisplay) {
+                  return null;
+                }
                 return (
                   <CardWrapper key={trip.id}>
                     <TravelCard key={trip.id} trip={trip} />
@@ -148,6 +147,15 @@ class TravelInfo extends React.Component {
             }}
           />
         </CardContainer>
+        <Route
+          exact
+          path="/travel-info/"
+          render={props => (
+            <StyledButton {...props} onClick={this.loadMore}>
+              Load More
+            </StyledButton>
+          )}
+        />
         <Route
           path="/travel-info/experiences/:id"
           render={props => {
